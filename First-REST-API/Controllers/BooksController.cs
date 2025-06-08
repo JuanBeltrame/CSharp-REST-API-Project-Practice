5c680a5cda5ce4096bc890d1bad2c835428646c0
Juan Beltrame <juan_beltrame@hotmail.com>
2025-06-08 13:20:59 -0300

    Creates Books API endpoint
    Adds a new API endpoint for managing books,
    allowing retrieval of all books or a specific
    book by ID.

---
+++ First-REST-API/Controllers/BooksController.cs
@@ -0,0 +1,37 @@
+﻿using First_REST_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static private List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "1984", Author = "George Orwell", YearPublished = 1949 },
+            new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", YearPublished = 1960 },
+            new Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", YearPublished = 1925 },
+            new Book { Id = 4, Title = "Brave New World", Author = "Aldous Huxley", YearPublished = 1932 },
+            new Book { Id = 5, Title = "Fahrenheit 451", Author = "Ray Bradbury", YearPublished = 1953 }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+    }
+}
